98ab166fb1ee127a350c12a13f0b5bf5b85317cf
Megankdcruz <s2836359@ed.ac.uk>
2025-09-24 14:58:23 +0100

    added figure and another comment

--- Process_data.R
+++ Process_data.R
@@ -5,3 +5,12 @@ library(tidyverse)
 install.packages("tidyverse")
 penguins<- read.table("data/penguin_data.txt", header = T)
 glimpse(penguins)
+#run linear regression
+model1<- lm(body_mass_g~flipper_length_mm, data = penguins)
+summary(model1)
+#create a plot in ggplot2:
+ggplot(penguins,aes(x=flipper_length_mm,y=body_mass_g, colour=species))+geom_point()+stat_smooth(method="lm")
+#save the plot in your/figs folder
+ggsave("figs/1_flipper_bodymass_regression.png")
+#this saves the last plot that was run!
+#this is another comment

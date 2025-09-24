05420944981366b804250b82f9cdd3f4307a7236
Megankdcruz <s2836359@ed.ac.uk>
2025-09-24 14:42:18 +0100

    penguin<-read.tables

--- Process_data.R
+++ Process_data.R
@@ -1,2 +1,7 @@
 penguins_data<-read.delim("Penguin_data.txt", header=TRUE)
 #delim is to read txt files as opposed to csv files 
+#Load the tidyverse library 
+library(tidyverse)
+install.packages("tidyverse")
+penguins<- read.table("data/penguin_data.txt", header = T)
+glimpse(penguins)
